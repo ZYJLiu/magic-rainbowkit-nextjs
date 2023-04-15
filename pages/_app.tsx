39f9d10c4d9fd8a665b8e7a5e910f1d346681366
John <johnzhengyuliu@gmail.com>
2023-04-15 03:50:45 -0500

    refactor magic-context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,48 +1,11 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
-import { useCallback, useEffect, useState, useMemo } from "react"
-import { Magic } from "magic-sdk"
-import { MagicContext } from "@/context/magic-context"
-import { Networks, formattedNetwork } from "../utils/networks"
-import Web3 from "web3"
+import MagicProvider from "@/context/magic-context"
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [magic, setMagic] = useState<Magic | null>(null)
-  const [web3, setWeb3] = useState<Web3 | null>(null)
-  const [selectedNetwork, setSelectedNetwork] = useState<Networks | null>(null)
-
-  const updateMagicInstance = useCallback((network: Networks) => {
-    setSelectedNetwork(network)
-    const magicInstance = new Magic(
-      process.env.NEXT_PUBLIC_MAGIC_API_KEY as string,
-      {
-        network: formattedNetwork(network),
-      }
-    )
-    setMagic(magicInstance)
-    setWeb3(new Web3((magicInstance as any).rpcProvider))
-  }, [])
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const storedNetwork =
-        (localStorage.getItem("network") as Networks | null) ||
-        Networks.Ethereum
-      setSelectedNetwork(storedNetwork)
-      updateMagicInstance(storedNetwork)
-    }
-  }, [])
-
   return (
-    <MagicContext.Provider
-      value={{
-        magic,
-        web3,
-        updateMagicInstance,
-        selectedNetwork,
-      }}
-    >
+    <MagicProvider>
       <Component {...pageProps} />
-    </MagicContext.Provider>
+    </MagicProvider>
   )
 }
